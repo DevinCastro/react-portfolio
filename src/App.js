8db034ef7b095de243c3a6f5145d9fc191836e9f
Devin Castro <dcrocker21@gmail.com>
2020-09-17 14:17:00 -0700

    navbar and contact page

--- src/App.js
+++ src/App.js
@@ -1,18 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
 import Home from './pages/Home'
 import Portfolio from './pages/Portfolio'
 import Contact from './pages/Contact'
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  NavbarText
+} from 'reactstrap'
+
 
 const App = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
   return (
     <Router>
       <div>
-        <nav>
-          <Link to="/">Home</Link>
-          <Link to="/portfolio">Portfolio</Link>
-          <Link to="/contact">Contact</Link>
-        </nav>
+        <Navbar color="light" light expand="md">
+          <NavbarBrand>Devin Castro</NavbarBrand>
+          <NavbarToggler onClick={toggle} />
+          <Collapse isOpen={isOpen} navbar>
+            <Nav className="mr-auto" navbar>
+              <NavItem>
+                <NavLink><Link to="/">Home</Link></NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink><Link to="/portfolio">Portfolio</Link></NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink><Link to="/contact">Contact</Link></NavLink>
+              </NavItem>
+            </Nav>
+          </Collapse>
+        </Navbar>
         <Switch>
           <Route exact path="/" component={Home} />
           <Route path="/portfolio" component={Portfolio} />
--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -1,8 +1,35 @@
 import React from 'react'
+import {
+  Button,
+  Form,
+  FormGroup,
+  Label,
+  Input,
+  FormText,
+  Container,
+  Row,
+  Col
+} from 'reactstrap'
 
 const Contact = () => {
   return (
-    <h1>This is the Contact page</h1>
+    <Container>
+          <Form>
+            <FormGroup>
+              <Label for="name">Name</Label>
+              <Input type="test" name="name" placeholder="Kenny Yang" />
+            </FormGroup>
+            <FormGroup>
+              <Label for="exampleEmail">Email</Label>
+              <Input type="email" name="email" id="exampleEmail" placeholder="with a placeholder" />
+            </FormGroup>
+            <FormGroup>
+              <Label for="message">Message</Label>
+              <Input type="text" name="message" />
+            </FormGroup>
+            <Button>Submit</Button>
+          </Form>
+    </Container>
   )
 }
 
